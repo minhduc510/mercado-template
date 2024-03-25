c627696821745e86b94a19c12da158ffa1ad2dc4
MinhDuc0510 <toladuc510@gmail.com>
2024-03-25 20:22:52 +0700

    update popup distribution

--- js/distribution-system.js
+++ js/distribution-system.js
@@ -1,9 +1,9 @@
 const overlay = document.querySelector('.overlay')
-const iconDeletePopup = document.querySelector('.popup-icon')
+const iconDeletePopup = document.querySelector('.popup-distribution-icon')
 
 function hiddenPopup() {
     overlay.style.display = 'none'
 }
 
-overlay.onclick = hiddenPopup
+// overlay.onclick = hiddenPopup
 iconDeletePopup.onclick = hiddenPopup
